25e838226864ea98b52bd9f702168109829d627c
Ruslan <84863005+Ruslon1@users.noreply.github.com>
2026-03-03 12:46:30 +0500

    define apis

--- lab_6/album-browser/src/app/services/album.service.ts
+++ lab_6/album-browser/src/app/services/album.service.ts
@@ -1,9 +1,33 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/internal/Observable";
+import { Album } from "../models/album.model";
 
 @Injectable({
     providedIn: 'root',
 })
 export class AlbumService {
     constructor(private http: HttpClient) {}
+
+    private readonly BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+    getAlbums(): Observable<Album[]>{
+        return this.http.get<Album[]>(`${this.BASE_URL}/albums`);
+    }
+
+    getAlbum(id: number): Observable<Album> {
+        return this.http.get<Album>(`${this.BASE_URL}/albums/${id}`);
+    }
+
+    getAlbumPhotos(id: number): Observable<any[]> {
+        return this.http.get<any[]>(`${this.BASE_URL}/albums/${id}/photos`);
+    }
+
+    updateAlbum(album: Album): Observable<Album> {
+        return this.http.put<Album>(`${this.BASE_URL}/albums/${album.id}`, album);
+    }
+
+    deleteAlbum(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.BASE_URL}/albums/${id}`);
+    }
 }
