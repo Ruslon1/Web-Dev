e0eec87afc95c16c481802d9c39c294704c0f2b8
Ruslan <84863005+Ruslon1@users.noreply.github.com>
2026-03-03 12:06:41 +0500

    routing

--- lab_6/album-browser/src/app/app.routes.ts
+++ lab_6/album-browser/src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home' },
+    { path: '', redirectTo: '/home' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'albums', component: AlbumsComponent },
+    { path: 'albums/:id', component: AlbumDetailsComponent },
+    { path: 'albums/:id/photos', component: AlbumPhotosComponent }
 ];
