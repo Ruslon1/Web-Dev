fbf79095df71635ab4c7194d399fba6d8071b3fd
Ruslan <84863005+Ruslon1@users.noreply.github.com>
2026-03-16 15:08:31 +0500

    lab 7 part 1

--- lab_6/album-browser/src/app/app.routes.ts
+++ lab_6/album-browser/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './faetures/about-component/about-component';
 import { AlbumsComponent } from './faetures/albums-component/albums-component';
 import { AlbumDetailsComponent } from './faetures/album-details-component/album-details-component';
 import { AlbumPhotosComponent } from './faetures/album-photos-component/album-photos-component';
+import { NameComponent } from './faetures/name-component/name-component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'albums', component: AlbumsComponent },
+    { path: 'name', component: NameComponent },
     { path: 'albums/:id', component: AlbumDetailsComponent },
     { path: 'albums/:id/photos', component: AlbumPhotosComponent }
 ];
---
+++ lab_6/album-browser/src/app/faetures/name-component/name-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-component',
+  imports: [],
+  templateUrl: './name-component.html',
+  styleUrl: './name-component.css',
+})
+export class NameComponent {
+
+}
